33896559652699847c0e7d8e1654f778527ca6bd
SallyMulupi <mulupisally2gmail.com>
2022-10-16 09:37:35 +0300

    generate classname app main

---
+++ client/src/components/main.js
@@ -0,0 +1,20 @@
+const Main = ({ activeNote, onUpdateNote }) => {
+    const onEditField = (field, value) => {
+      onUpdateNote({
+        ...activeNote,
+        [field]: value,
+        lastModified: Date.now(),
+      });
+    };
+    if (!activeNote) return <div className="no-active-note">No Active Note</div>;
+    return (
+        <div className="app-main">
+          <div className="app-main-note-edit">
+            <input
+              type="text"
+              id="title"
+              placeholder="Note Title"
+              value={activeNote.title}
+              onChange={(e) => onEditField("title", e.target.value)}
+              autoFocus
+            />
